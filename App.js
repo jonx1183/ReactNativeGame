6029e772a9baf00887fbef02db206361567c5db3
jonx1183 <jonx1183@stud.kea.dk>
2023-11-06 12:54:00 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,138 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Platform } from 'react-native';
+import { useState, useEffect } from 'react';
+import { app, database } from './firebase';
+import { addDoc, collection } from 'firebase/firestore';
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword} from "firebase/auth"
+import { initializeAuth, getReactNativePersistence, signOut } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+// web
+// device / emulator
+let auth
+if(Platform.OS === 'web'){
+auth = getAuth(app)
+}else {
+auth = initializeAuth(app, {
+persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+})
 }
 
+
+export default function App() {
+const [enteredEmail, setEnteredEmail] = useState("jon@m.dk")
+const [enteredPassword, setEnteredPassword] = useState("123456")
+const [userId, setUserId] = useState(null)
+const [enteredText, setenteredText] = useState("type here")
+
+
+useEffect(() =>{
+const auth_ = getAuth()
+const unsubscribe = onAuthStateChanged(auth_, (currentUser) => {
+if(currentUser){
+setUserId(currentUser.uid)
+}else{
+setUserId(null)
+}
+})
+return () => unsubscribe() // kaldes når componenten unmountes.
+},[])
+
+
+async function addDocument(){
+try{
+await addDoc(collection(database, userId),{
+text:enteredText
+})
+}catch(error){
+console.log("error addDocument " + error)
+}
+}
+
+
+async function login(){
+try{
+const userCredential = await signInWithEmailAndPassword(auth, enteredEmail, enteredPassword)
+console.log("logged ind " + userCredential.user.uid)
+}catch(error){}
+}
+
+
+async function signup(){
+try{
+const userCredential = await createUserWithEmailAndPassword(auth, enteredEmail, enteredPassword)
+console.log("oprettet ny bruge " + userCredential.user.uid)
+}catch(error){}
+}
+
+
+async function sign_out(){
+await signOut(auth)
+}
+
+
+
+
+return (
+<View style={styles.container}>
+{ !userId &&
+<>
+<Text>Login</Text>
+<TextInput
+onChangeText={newText => setEnteredEmail(newText)}
+value={enteredEmail}
+/>
+<TextInput
+onChangeText={newText => setEnteredPassword(newText)}
+value={enteredPassword}
+/>
+<Button
+title='Log in'
+onPress={login}
+/>
+
+
+<TextInput
+onChangeText={newText => setEnteredEmail(newText)}
+value={enteredEmail}
+/>
+<TextInput
+onChangeText={newText => setEnteredPassword(newText)}
+value={enteredPassword}
+/>
+<Button
+title='Signup'
+onPress={signup}
+/>
+</>
+}
+{ userId &&
+<>
+<TextInput
+onChangeText={newText => setenteredText(newText)}
+value={enteredText}
+/>
+<Button
+title='Add new Document'
+onPress={addDocument}
+/>
+<Button
+title='Sign out'
+onPress={sign_out}
+/>
+</>
+}
+</View>
+);
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+flex: 1,
+backgroundColor: '#fff',
+alignItems: 'center',
+justifyContent: 'center',
+},
 });
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// firebase.js  skift til dine credentials
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2RCOa6PiG990HjVY_BHr9ftPErtERWRw",
+  authDomain: "jonsproject-2b6fc.firebaseapp.com",
+  projectId: "jonsproject-2b6fc",
+  storageBucket: "jonsproject-2b6fc.appspot.com",
+  messagingSenderId: "583412275457",
+  appId: "1:583412275457:web:fd947feaff5c697fc1e300"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+export { database, app }
